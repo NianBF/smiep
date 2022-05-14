c30cac53cb13f9777a31d7b98a88d73b04eed640
= <=>
2022-05-14 13:40:13 -0500

    NianBF: update sources from texts css and js

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <meta http-equiv='X-UA-Compatible' content='ie=edge'>
     <link rel="icon" type="image/png" href="img/favicon.png" sizes="any">
+    <link rel="stylesheet" type="text/css" href="public/css/stylesIndex.css">
     <title>SMIEP</title>
 </head>
 <body>
