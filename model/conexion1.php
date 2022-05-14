48d3ec8f0b575f8a48faa42d641cdf788d84acb5
= <=>
2022-05-14 14:14:00 -0500

    NianBF: update conection DB Heroku

--- model/conexion1.php
+++ model/conexion1.php
@@ -10,7 +10,7 @@ class Db
 	public static function conectar()
 	{
 		$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-		self::$conexion = new PDO('mysql:host=localhost;dbname=smiep', 'root', '', $pdo_options);
+		self::$conexion = new PDO('mysql:host=us-cdbr-east-05.cleardb.net; dbname=heroku_619553700a45b98', 'bcc4441154c3a9', 'b4a4c01d', $pdo_options);
 		return self::$conexion;
 	}
 }
