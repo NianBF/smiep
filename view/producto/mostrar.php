6eb285ef413604811e1ea959c42fde3834151bb7
cristian1701 <criistiian1701@gmail.com>
2022-06-01 22:39:53 -0500

    cristian1701: Update styles view product

--- view/producto/mostrar.php
+++ view/producto/mostrar.php
@@ -22,32 +22,39 @@ $listaProducto=$crud->mostrar();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" href="../../img/favicon.png" sizes="any">
     <title>Mostrar producto</title>
-    <link rel="stylesheet" type="text/css" href="../../public/css/catalogo.css">
+    <link rel="stylesheet" type="text/css" href="../../public/css/producto.css">   
+    <script src="https://kit.fontawesome.com/2c36e9b7b1.js"></script> 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" 
+    integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" 
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 </head>
 
 <body>
     <table>
         <header>
-            <a href="../inicio/menu.php"><span class="icon">
-                    <figure class=""><img src="../../img/favicon.png" alt="Logo SMIEP" width="180rem"></figure>
-            </a>
-            <h1 class="titulo">S.M.I.E.P</h1>
-            <h3 class="subtitulo">Software de Manejo de Inventarios para Empresas Pequeñas</h3>
+            <div class="logo">
+                <img src="../../img/favicon.png" alt="Logo SMIEP" width="150rem">
+                <h1 class="titulo">S.M.I.E.P</h1>
+                <h3 class="subtitulo">Software de Manejo de Inventarios para Empresas Pequeñas</h3>
+            </div>
         </header>
         <br>
-        <br>
-        <br>
         <hr>
-        <br>
-        <h4>Productos</h4>
-        <a href="agregarProducto.php" class="agregar">Agregar</a>
-        <br>
-        <br>
-        <div class="buscar">
-        <label for="filtrar-tabla"></label>
-		<input type="text" name="filtrar-tabla" id="filtrar-tabla" placeholder="Productos" class="buscar1">
-        </div>
+        <div id="main-container">
+        <thead>
+            <tr>
+                <th colspan="7">Listado de Productos <a href='agregarProducto.php'><button type="button" id="agregar"><i class="fa-solid fa-plus"></i>  Nuevo Producto</button></a>
+                <a href='../inicio/menu.php'><button type="button" id="volver"><i class="fa-solid fa-arrow-rotate-left"></i> Volver</button></a></th>
+            </tr>
+            <tr id="lis">
+                <th colspan="7">
+             <div class="buscar">
+                <label for="filtrar-tabla"></label>
+		        <input type="text" name="filtrar-tabla" id="filtrar-tabla" placeholder="Productos" class="buscar1">
+             </div>
+                </th>
+            </tr>
 
         <tr>
             <th>ID</th>
@@ -55,10 +62,9 @@ $listaProducto=$crud->mostrar();
             <th>Nombre</th>
             <th>Precio</th>
             <th>Stock</th>
-            <th>Actualizar</th>
-            <th>Eliminar</th>
-
+            <th  colspan="2">Opciones</th>
         </tr>
+        </thead>
 
         <?php foreach ($listaProducto as $Producto) {?>
         <tr class="producto">
@@ -79,17 +85,19 @@ $listaProducto=$crud->mostrar();
             <td class="cantidad">
                 <?php echo $Producto->getCantidadDisp() ?>
             </td>
-            <td><a class="editar" id="btnActualizar" name="btnActualizar"
-                    href="actualizar.php?id_prod=<?php echo $Producto->getId_prod()?>&accion=a">Actualizar</a>
+            <td><a id="btnActualizar" name="btnActualizar"
+                    href="actualizar.php?id_prod=<?php echo $Producto->getId_prod()?>&accion=a"><button type="button"><i class="fa-solid fa-pencil"></i></button></a>
             </td>
-            <td><a type="submit" class="btn btn-outline-light eliminar" id="btnEliminar" name="btnEliminar"
-                    href="../../controller/productoCtrl.php?id_prod=<?php echo $Producto->getId_prod()?>&accion=e">Eliminar</a>
+            <td><a type="submit" id="btnEliminar" name="btnEliminar"
+                    href="../../controller/productoCtrl.php?id_prod=<?php echo $Producto->getId_prod()?>&accion=e"><button type="button" id="eliminar"><i class="fa-solid fa-trash-can"></i></button></a>
             </td>
+            
         </tr>
+        </div>
         <?php }?>
 
     </table>
-        <script src="../../public/js/producto/filtrarProducto.js"></script>
+        <script src="../../public/js/producto/filtrarProducto.js"></>
 </body>
 
 </html>
