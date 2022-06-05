09918716810a796ee6bb73b78b9c1c377359d3e0
= <=>
2022-06-05 13:50:57 -0500

    NianBF: create folder 'modules' for add 'vender' and 'reportes', now generate reports in PDF from table 'producto'

---
+++ module/reportes/product.php
@@ -0,0 +1,28 @@
+<?php
+	include 'plantilla.php';
+	require '../../model/conexion.php';
+	
+    $con = Conexion::getConection();
+	$query = "SELECT nombreProd, precio, cantidadDisp FROM producto";
+	$resultado = $con->query($query);
+	
+	$pdf = new PDF();
+	$pdf->AliasNbPages();
+	$pdf->AddPage();
+	
+	$pdf->SetFillColor(232,232,232);
+	$pdf->SetFont('Arial','B',12);
+	$pdf->Cell(70,6,'PRODUCTO',1,0,'C',1);
+	$pdf->Cell(20,6,'PRECIO',1,0,'C',1);
+	$pdf->Cell(70,6,'DISPONIBLE',1,1,'C',1);
+	
+	$pdf->SetFont('Arial','',10);
+	
+	while($row = $resultado->fetch(PDO::FETCH_ASSOC))
+	{
+		$pdf->Cell(70,6,$row['nombreProd'],1,0,'C');
+		$pdf->Cell(20,6,$row['precio'],1,0,'C');
+		$pdf->Cell(70,6,$row['cantidadDisp'],1,1,'C');
+	}
+	$pdf->Output();
+?>
