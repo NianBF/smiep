46199e3d2a2e1e02548d471a6c41934a49745fad
JEstu <jeigestupin@misena.edu.co>
2022-06-10 22:44:59 -0500

    Jestu: Report files are created

--- module/reportes/plantilla.php
+++ module/reportes/plantilla.php
@@ -5,18 +5,25 @@
 	{
 		function Header()
 		{
-			$this->Image('../../img/favicon.png', 5, 5, 30 );
+			$this->Image('../../img/favicon.png',5, 5, 30 );
 			$this->SetFont('Arial','B',15);
 			$this->Cell(30);
 			$this->Cell(120,10, 'Reporte De Estados',0,0,'C');
 			$this->Ln(20);
 		}
-		
+		function User(){
+			session_start();
+			date_default_timezone_set('America/Bogota');
+			$this->Cell(65,10,date("Y-m-d H:i:s"),0,0,'C');
+			$this->Cell(65,10,"Usuario: ".$_SESSION["userName"],0,0,'C');
+		}
 		function Footer()
 		{
 			$this->SetY(-15);
 			$this->SetFont('Arial','I', 8);
-			$this->Cell(0,10, 'Pagina '.$this->PageNo().'/{nb}',0,0,'C' );
+			$this->User();
+			$this->Cell(65,10, 'Pagina '.$this->PageNo().'/{nb}',0,0,'C' );
+			
 		}		
 	}
 ?>
--- view/reportes/mostrar.php
+++ view/reportes/mostrar.php
@@ -4,12 +4,6 @@ if($_SESSION['email'] == null or $_SESSION["userName"]== null or
 $_SESSION["pass"] == null ){
     header("location:../../index.php");
 }else{
-require_once('../../model/productoCRUD_Mdl.php');
-require_once('../../model/productoMdl.php');
-$crud=new CrudProducto();
-$Producto= new Producto();
-
-$listaProducto=$crud->mostrar();
 ?>
 
 
@@ -21,7 +15,7 @@ $listaProducto=$crud->mostrar();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" href="../../img/favicon.png" sizes="any">
-    <title>Mostrar Reportes</title>
+    <title>Reportes</title>
     <link rel="stylesheet" type="text/css" href="../../public/css/producto.css">   
     <script src="https://kit.fontawesome.com/2c36e9b7b1.js"></script> 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" 
@@ -59,24 +53,28 @@ $listaProducto=$crud->mostrar();
         </tr>
         <tr>
             <td>Categoria</td>
-            <td><a href='#' target="_BLANK"><button type="button"><i class="fa-solid fa-eye"></i></button></a></td>
+            <td><a href='../../module/reportes/categoria.php' target="_BLANK"><button type="button"><i class="fa-solid fa-eye"></i></button></a></td>
         </tr>
         <tr>
             <td>Usuarios</td>
-            <td><a href='#' target="_BLANK"><button type="button"><i class="fa-solid fa-eye"></i></button></a></td>
+            <td><a href='../../module/reportes/usuario.php' target="_BLANK"><button type="button"><i class="fa-solid fa-eye"></i></button></a></td>
         </tr>
         <tr>
-            <td>Personas</td>
-            <td><a href='#' target="_BLANK"><button type="button"><i class="fa-solid fa-eye"></i></button></a></td>
+            <td>Clientes</td>
+            <td><a href='../../module/reportes/cliente.php' target="_BLANK"><button type="button"><i class="fa-solid fa-eye"></i></button></a></td>
         </tr>
         <tr>
             <td>Proveedores</td>
-            <td><a href='#' target="_BLANK"><button type="button"><i class="fa-solid fa-eye"></i></button></a></td>
+            <td><a href='../../module/reportes/provee.php' target="_BLANK"><button type="button"><i class="fa-solid fa-eye"></i></button></a></td>
+        </tr>
+        <tr>
+            <td>Ventas</td>
+            <td><a href='../../module/reportes/venta.php' target="_BLANK"><button type="button"><i class="fa-solid fa-eye"></i></button></a></td>
         </tr>
         </div>
 
     </table>
-        <script src="../../public/js/producto/filtrarProducto.js"></>
+        <script src="../../public/js/producto/filtrarProducto.js"></script>
 </body>
 
 </html>
