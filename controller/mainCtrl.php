dcb08c22b6c466f67d50611e3c7ca7d786617e8d
NicStevenP <nsperez02@misena.edu.co>
2022-06-13 10:42:18 -0500

    NicStev: msg: login incorrect

--- controller/mainCtrl.php
+++ controller/mainCtrl.php
@@ -1,9 +1,18 @@
+
+<html>
+<head>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+
+</head>	
+<body>
+
 <?php
 
 session_start();
 if (isset($_SESSION["email"]) && isset($_SESSION["userName"]) && isset($_SESSION["pass"]))
 {
-
+        
     require_once("model/validarMdl.php");
     $validar = new Validar();
     $validar->validarDatos();
@@ -17,10 +26,20 @@ else
     if (isset($_SESSION["error"]))
     {
 
-        echo "<script>alert('Usuario, email o contraseña incorrecto')</script>";
+        echo "<script>Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Ususario, correo o contraseña incorrectos!'
+          })</script>";
         unset($_SESSION["error"]);
 
     }
 
     include_once("view/index.php");
 }
+
+?>
+
+
+</body>
+</html>
