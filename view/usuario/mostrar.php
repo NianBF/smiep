136b912109a9226cb3def21554bc859079d57ab5
cristian1701 <criistiian1701@gmail.com>
2022-06-23 12:38:59 -0500

    Cristian1701: Update style view user

--- view/usuario/mostrar.php
+++ view/usuario/mostrar.php
@@ -24,32 +24,40 @@ $listaUsuario=$crud->mostrar();
 
 	<title>Mostrar Usuarios</title>
 	
-	<link rel="stylesheet" href="../../public/css/producto.css">
+	<link rel="stylesheet" href="../../public/css/usuario.css">
 
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" 
     integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" 
     crossorigin="anonymous" referrerpolicy="no-referrer" />	
 
 </head>
-<body>
-	<table>
-		<header>
-			<div class="logo">
-				<img src="../../img/favicon.png" alt="Logo SMIEP" width="150rem">
-				<h1 class="titulo">S.M.I.E.P</h1>
-				<h3 class="subtitulo">Software de Manejo de Inventarios para Empresas Pequeñas</h3>
-			</div>
-		</header>
+<header>
+<table>
+        <div class="header__superior">
+            <div class="logo">
+                <img src="../../img/favicon.png" alt="">
+            </div>
+            <div class="contenedor">
+            <section class="titulito">
+                <h1 class="title">S.M.I.E.P</h1>
+                <h3 class="nameEmp">Software de Manejo de Inventarios para Empresas Pequeñas</h3>
+                <h3 class="nameEmp2">Software de Manejo <br>de Inventarios <br>para Empresas Pequeñas</h3>
+            </section>
+        </div>
+        </div>
+        </header>
 			<br>
 			<hr>
+			<br>
 			<div id="main-container">
 			<thead>
 			<tr>
-				<th colspan="7">Listado de Usuarios <a href='ingresar.php'><button type="button" id="agregar"><i class="fa-solid fa-plus"></i>  Nuevo Usuario</button></a>
-				<a href='../inicio/menu.php'><button type="button" id="volver"><i class="fa-solid fa-arrow-rotate-left"></i> Volver</button></a></th>
-			</tr>
+            <th>Listado de Usuarios</th>
+            	<th colspan="6" class="bot1"><a href='ingresar.php'><button type="button" id="agregar"><i class="fa-solid fa-plus"></i>  Agregar</button></a>
+            	<a class="bot1 "href='../inicio/menu.php'><button type="button" id="volver"><i class="fa-solid fa-arrow-rotate-left"></i> Volver</button></a></th>
+        	</tr>
 			<tr id="lis">
-			<th colspan="7">
+			<th colspan="6">
 				<div class="buscar">
 					<label for="filtrar-tabla"></label>
 					<input type="text" name="filtrar-tabla" id="buscar1" placeholder="ID Usuario" class="buscar1">
@@ -61,13 +69,17 @@ $listaUsuario=$crud->mostrar();
 					<input type="text" name="filtrar-tabla" id="buscar3" placeholder="Rol" class="buscar1">
 				</div>
 			</th>
+			<th colspan="2">
+                <a id="mod" class="mod" onclick="cambiarModo()"><span id="id-moon" class="btn-mode moon"><i class="fas fa-sun"></i></span>/<span  id="id-sun" class="btn-mode sun active"><i class="fas fa-moon"></i></span></a>
+                <script type="text/javascript" src="../../public/js/darkMode/darkMode.js"></script>
+            </th>
 			</tr>
-			<tr>
-				<th>ID</th>
-				<th>Nombre</th>
-				<th>Usuario</th>
-				<th>Correo</th>
-				<th>Rol</th>
+			<tr class="tb">
+				<th class="tb">ID</th>
+				<th class="to">Nombre</th>
+				<th class="tb">Usuario</th>
+				<th class="tx">Correo</th>
+				<th class="tz">Rol</th>
 				<th colspan="2">Opciones</th>
 			</tr>
 			</thead>
@@ -86,6 +98,9 @@ $listaUsuario=$crud->mostrar();
 				<?php }?>
 			</div>
 		</table>
+		<footer class="footer">
+        <p>© S.M.I.E.P | 2022</p>
+    	</footer>
 		
 		<script src="../../public/js/usuario/filtrarUsuarios.js"></script>
 		
