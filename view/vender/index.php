783fde2dbde26986c5c1cb482b91aa469b028cec
NianBF <nianbf07@gmail.com>
2022-06-24 15:37:35 -0500

    NianBF: update documentation

--- view/vender/index.php
+++ view/vender/index.php
@@ -6,12 +6,10 @@ $_SESSION["pass"] == null)
     header("location:../../index.php");
 }
 else
-{
-    include 'Configuracion.php';
+{    include 'Configuracion.php';
 ?>
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -23,51 +21,39 @@ else
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <style>
-        .container {
-            padding: 20px;
-        }
-
-        .cart-link {
-            width: 100%;
-            text-align: right;
-            display: block;
-            font-size: 22px;
-        }
+    .container{padding: 20px;}
+    .cart-link{width: 100%;text-align: right;display: block;font-size: 22px;}
     </style>
 </head>
 </head>
-
 <body>
-    <div class="contenedor">
-        <span class="icon">
-            <figure class=""><img src="../../img/favicon.png" alt="Logo SMIEP" width="230px"></figure>
-        </span>
-
+<div class="contenedor">
+        <span class="icon"><figure class=""><img src="../../img/favicon.png" alt="Logo SMIEP" width="230px"></figure></span>
+        
         <!--<header class="header">
             <section>
                 <h1 class="title">S.M.I.E.P</h1>
                 <h3 class="nameEmp">Software de Manejo de Inventarios para Empresas Pequeñas</h3>
             </section>-->
         </header>
-    </div>
-    <div class="container">
-        <div class="panel panel-default">
-            <div class="panel-heading">
+</div>
+<div class="container">
+<div class="panel panel-default">
+<div class="panel-heading"> 
 
-                <ul class="nav nav-pills">
-                    <li role="presentation" class="active"><a href="index.php">Inicio</a></li>
-                    <li role="presentation"><a href="VerCarta.php">Ver Carta</a></li>
-                    <li role="presentation"><a href="Pagos.php">Pagos</a></li>
-                    <li role="presentation"><a href="../inicio/menu.php">Volver</a></li>
-                </ul>
-            </div>
+<ul class="nav nav-pills">
+  <li role="presentation" class="active"><a href="index.php">Inicio</a></li>
+  <li role="presentation"><a href="VerCarta.php">Ver Carta</a></li>
+  <li role="presentation"><a href="Pagos.php">Pagos</a></li>
+  <li role="presentation"><a href="../inicio/menu.php">Volver</a></li>
+</ul>
+</div>
 
-            <div class="panel-body">
-                <h1>Productos</h1>
-                <a href="VerCarta.php" class="cart-link" title="Ver Carta"><i
-                        class="glyphicon glyphicon-shopping-cart"></i></a>
-                <div id="products" class="row list-group">
-                    <?php
+<div class="panel-body">
+    <h1>Productos</h1>
+    <a href="VerCarta.php" class="cart-link" title="Ver Carta"><i class="glyphicon glyphicon-shopping-cart"></i></a>
+    <div id="products" class="row list-group">
+        <?php
     //get rows query
     $query = $db->query("SELECT * FROM producto ORDER BY id_prod DESC LIMIT 20");
     if ($query->num_rows > 0)
@@ -75,50 +61,38 @@ else
         while ($row = $query->fetch_assoc())
         {
 ?>
-                    <div class="item col-lg-4">
-                        <div class="thumbnail">
-                            <div class="caption">
-                                <h4 class="list-group-item-heading">
-                                    <?php echo $row["nombreProd"]; ?>
-                                </h4>
-                                <figure><img src="<?php echo $row[" imgProd"]; ?>" alt="
-                                    <?php echo $row["nombreProd"]; ?>" width="20%">
-                                </figure>
-                                <p class="list-group-item-text">
-                                    <?php echo $row["descripcion"]; ?>
-                                </p>
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <p class="lead">
-                                            <?php echo '$' . $row["precio"] . ' COP'; ?>
-                                        </p>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <a class="btn btn-success"
-                                            href="AccionCarta.php?action=addToCart&id_prod=<?php echo $row[" id_prod"];
-?>">Agregar a la Carta</a>
-                                    </div>
-                                </div>
-                            </div>
+        <div class="item col-lg-4">
+            <div class="thumbnail">
+                <div class="caption">
+                    <h4 class="list-group-item-heading"><?php echo $row["nombreProd"]; ?></h4>
+                    <figure><img src="<?php echo $row["imgProd"]; ?>" alt="<?php echo $row["nombreProd"]; ?>" width="20%"></figure>
+                    <p class="list-group-item-text"><?php echo $row["descripcion"]; ?></p>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <p class="lead"><?php echo '$' . $row["precio"] . ' COP'; ?></p>
+                        </div>
+                        <div class="col-md-6">
+                            <a class="btn btn-success" href="AccionCarta.php?action=addToCart&id_prod=<?php echo $row["id_prod"]; ?>">Agregar a la Carta</a>
                         </div>
                     </div>
-                    <?php
+                </div>
+            </div>
+        </div>
+        <?php
         }
     }
     else
     { ?>
-                    <p>Producto(s) no existe(n).....</p>
-                    <?php
+        <p>Producto(s) no existe(n).....</p>
+        <?php
     }?>
-                </div>
-            </div>
-            <div class="panel-footer">SMIEP</div>
-        </div>
-        <!--Panek cierra-->
-
     </div>
+        </div>
+ <div class="panel-footer">SMIEP</div>
+ </div><!--Panek cierra-->
+ 
+</div>
 </body>
-
 </html>
 <?php
 }?>
