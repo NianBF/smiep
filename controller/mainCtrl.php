0a7d008b6975f75c631db8e77bb8006be91d883d
NianBF <nianbf07@gmail.com>
2022-08-04 14:21:31 -0500

    NianBF: any changes in frontend

--- controller/mainCtrl.php
+++ controller/mainCtrl.php
@@ -3,7 +3,7 @@
 <head>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
-
+<meta name="theme-color" content="#339999">
 </head>	
 <body>
 
