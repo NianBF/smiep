6f343300bf4ef1838304bce34a6e0c9b0a3dc52a
NianBF <nianbf07@gmail.com>
2022-08-19 16:23:14 -0500

    NianBF: Create protocol for recovery password, this function await for test

--- view/login/recoveryPass/sendEmail.php
+++ view/login/recoveryPass/sendEmail.php
@@ -6,27 +6,27 @@ $id_doc = $_GET["id_doc"];
 
 $destinatario = $recoveryEmail; 
 $asunto = "Este mensaje es de prueba"; 
-$cuerpo = ' 
-<html> 
+$cuerpo =  
+<<<HTML
 <head> 
    <title>Prueba de correo</title> 
 </head> 
 <body> 
-<h1>Hola, usuario</h1> 
+<h1>Hola, $userName</h1> 
 <p> 
 <b>Bienvenidos a mi correo electrónico de prueba</b>. Estoy encantado de tener tantos lectores. Este cuerpo del mensaje es del artículo de envío de mails por PHP. Habría que cambiarlo para poner tu propio cuerpo. Por cierto, cambia también las cabeceras del mensaje. 
 </p> 
 </body> 
-</html> 
-'; 
+HTML; 
 
 //para el envío en formato HTML 
 $headers = "MIME-Version: 1.0\r\n"; 
 $headers .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
 
 //dirección del remitente 
-$headers .= "From: Nicolas Bautista <narufansofficial@gmail.com>"; 
- 
+$headers .= "From: Nicolas Bautista <narufansofficial@gmail.com>\r\n"; 
+$headers .= "To: Nicolas Bautista <$destinatario>\r\n";   
+
 if(mail($destinatario,$asunto,$cuerpo,$headers)) {
     echo ("Mensaje enviado");
 }else {
