40d75f050947986352e193224c3fa4861c04b1e7
NianBF <nianbf07@gmail.com>
2022-08-19 16:06:23 -0500

    NianBF: Create protocol for recovery password, this function await for test

--- view/login/recoveryPass/sendEmail.php
+++ view/login/recoveryPass/sendEmail.php
@@ -12,7 +12,7 @@ $cuerpo = '
    <title>Prueba de correo</title> 
 </head> 
 <body> 
-<h1>Hola, <?php echo("$userName") ?></h1> 
+<h1>Hola, usuario</h1> 
 <p> 
 <b>Bienvenidos a mi correo electrónico de prueba</b>. Estoy encantado de tener tantos lectores. Este cuerpo del mensaje es del artículo de envío de mails por PHP. Habría que cambiarlo para poner tu propio cuerpo. Por cierto, cambia también las cabeceras del mensaje. 
 </p> 
