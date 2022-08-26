73cad69a38ede473a8efc8866dec87abd8adcb10
NianBF <nianbf07@gmail.com>
2022-08-26 11:07:21 -0500

    NianBF: Update code in frontend for best organization and clean code

--- view/login/recoveryPass/recoveryPass.php
+++ view/login/recoveryPass/recoveryPass.php
@@ -9,6 +9,7 @@
     <title>SMIEP</title>
     <link rel="stylesheet" href="../../../public/css/recoveryPass/recoveryPass.css">
     <link rel="stylesheet" href="../../../public/css/plantillas/header1.css">
+    <link rel="stylesheet" href="../../../public/css/plantillas/forms.css">
 </head>
 <body>
     <header>
@@ -19,13 +20,13 @@
         <article>
             <p>Por favor, llene los siguientes datos con la información exacta con la que está registrado en <strong>S.M.I.E.P</strong>, así podremos validar que sea parte de nuestra base de datos y recuperar de forma correcta su acceso al sistema.</p></br>
         </article>
-        <div class="loseUser">
+        <div class="contForm">
             <form action="../../../controller/recoveryPassCtrl.php" method="post">
                 <fieldset class="anuncio">
                     <legend>Advertencia</legend>
                     <div>
                         <article>
-                            <p>Este proceso solo puede ser realizado para usuarios con permisos especiales. <strong>S.M.I.E.P</strong> no se hace responsable de la filtración, fuga o perdida de datos que pueda provocar usted al hacer uso de este apartado.</p></br>
+                            <p>Este proceso solo puede ser realizado por usuarios con permisos especiales. <strong>S.M.I.E.P</strong> no se hace responsable de la filtración, fuga o perdida de datos que pueda provocar usted al hacer uso de este apartado.</p></br>
                             <p><strong>S.M.I.E.P</strong> obtiene sus datos mediante información brindada por su empresa, recuerde que todos estos datos son confidenciales entre usted, la empresa y <strong>S.M.I.E.P</strong>, por lo que puede incurrir en una violación grave al compartir información confidencial con personas externas. Además, su información se encuentra protegida y solo puede ser manipulada por personas con un vínculo con la empresa para la que trabaja.</p>
                         </article>
                     </div>
